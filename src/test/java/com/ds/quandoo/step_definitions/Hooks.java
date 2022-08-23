02685dc3d32841f4cc914c25b353ce0b3f5d81c3
Dmitrii Speranskii <dmitriisperanskii@Dmitriis-MBP.fritz.box>
2022-08-23 15:22:55 +0200

    Updated log4j dependency and step definitions

--- src/test/java/com/ds/quandoo/step_definitions/Hooks.java
+++ src/test/java/com/ds/quandoo/step_definitions/Hooks.java
@@ -5,8 +5,9 @@ import com.ds.quandoo.managers.AppManager;
 import com.ds.quandoo.managers.PageManager;
 import com.ds.quandoo.model.User;
 import com.ds.quandoo.world.World;
-import cucumber.api.Scenario;
-import cucumber.api.java8.En;
+import io.cucumber.java8.En;
+import io.cucumber.java8.Scenario;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import static com.ds.quandoo.util.PropertyLoader.loadProperty;
@@ -24,7 +25,7 @@ public class Hooks implements En {
 	public Hooks(World world) {
 
         // Before web tests hooks
-        Before(new String[]{"@web"}, (Scenario scenario) -> {
+        Before("@web", (Scenario scenario) -> {
             am = AppManager.getAppManager();
             am.startApp();
             world.setDriver(am.getDriver());
@@ -32,18 +33,18 @@ public class Hooks implements En {
         });
 
         // Scenario specific hooks
-        Before(new String[]{"@login"}, (Scenario scenario) -> {
+        Before("@login", (Scenario scenario) -> {
             String userName = loadProperty("username");
             String password = loadProperty("password");
             world.setUser(new User().setUserCredentials(userName, password));
         });
 
         // After web tests hooks
-        After(new String[]{"@web"}, (Scenario scenario) -> {
+        After("@web", (Scenario scenario) -> {
             if (scenario.isFailed()) {
                 // Takes a screenshot and embeds it in the report...
                 final byte[] screenshot = ((TakesScreenshot) am.getDriver()).getScreenshotAs(OutputType.BYTES);
-                scenario.embed(screenshot, "image/png");
+                scenario.attach(screenshot, "image/png", "screenshot");
             }
             am.closeApp();
         });
